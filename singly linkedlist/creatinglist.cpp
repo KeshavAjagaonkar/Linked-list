683cf122c958696eb30a58cbb1db263f0ca99ae7
Keshav_ajagaonkar <keshavvamanajagaonkar@gmail.com>
2025-07-18 16:03:22 +0530

    add modularity using different function to reduce repetative line of code

--- singly linkedlist/creatinglist.cpp
+++ singly linkedlist/creatinglist.cpp
@@ -12,14 +12,31 @@ struct Node
         next = nullptr;
     }
 };
-
-
-
+void printList(Node* head){
+    Node *temp = head;
+    while(temp!=nullptr){
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+}
+// here the change that may be small but very good impact when we pass it by reference . it avoid problem when the list is empty 
+void insertAtTail(Node* &head,int val){
+    Node *temp = head;
+    if(head==nullptr){
+        head = new Node(val);
+        return;
+    }
+    while(temp->next!=nullptr){
+        temp = temp->next;
+    }
+    temp->next = new Node(val);
+}
 
 
 int main(){
     Node *first = new Node(10);
     first->next = new Node(20);
-    cout << first->data<<endl;
-    cout << first->next->data;
+
+    insertAtTail(first, 30);
+    printList(first);
 }
